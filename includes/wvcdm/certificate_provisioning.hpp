7de3966c8c6f614e636bf880e0d9c5cf2a95dfd5
EiNSTeiN_ <einstein@g3nius.org>
2014-05-24 22:44:17 -0400

    fix class declaration

--- includes/wvcdm/certificate_provisioning.hpp
+++ includes/wvcdm/certificate_provisioning.hpp
@@ -5,13 +5,18 @@ namespace wvcdm {
   
   class CertificateProvisioning {
   public:
+    CertificateProvisioning(void) {}
+
     /* Create a provisioning request, sign it with derived keys, and return the 
       request as a query string. */
-    static int GetProvisioningRequest(SecurityLevel security_level, 
+    int GetProvisioningRequest(SecurityLevel security_level,
       std::string *query_string, std::string *default_provisioning_server);
 
     /* Take a provisioning request serialized as string and return a query string. */
-    static int ComposeJsonRequestAsQueryString(CryptoSession *session, 
+    int ComposeJsonRequestAsQueryString(CryptoSession *session,
       const std::string &provisioning_msg, std::string *query_string);
+
+  public:
+    CryptoSession session;
   };
 };
